b2b04a317eee08ca213362a92f53f6d355fd3455
Gavin Joyce <gavinjoyce@gmail.com>
2019-06-27 11:06:43 +0100

    add spike of boot time error

--- app/routes/apps/app.js
+++ app/routes/apps/app.js
@@ -1,6 +1,9 @@
 import Route from '@ember/routing/route';
+import { inject as service } from '@ember/service';
 
 export default Route.extend({
+  bootService: service(),
+
   beforeModel() {
     console.log('beforeModel: apps.app');
   },
@@ -8,6 +11,11 @@ export default Route.extend({
   actions: {
     error(error, transition) {
       console.log('app.app error handler', error, transition);
+      console.log('boot', this.bootService);
+
+      if (!this.bootService.hasBooted) {
+        this.bootService.set('showBootError', true);
+      }
       return true;
     }
   }
--- app/services/boot-service.js
+++ app/services/boot-service.js
@@ -1,4 +1,15 @@
 import Service from '@ember/service';
+import { later } from '@ember/runloop';
 
 export default Service.extend({
+  hasBooted: false,
+  showBootError: false,
+
+  init() {
+    this._super(...arguments);
+
+    later(this, function() {
+      this.set('hasBooted', true); //simulate the staged boot process
+    }, 1000);
+  },
 });
